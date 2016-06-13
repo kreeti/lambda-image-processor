660dbbe8df3eb329304bee5ff4bbbacf574dd783
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2016-06-13 13:07:42 +0530

    implementing promise

--- index.js
+++ index.js
@@ -9,148 +9,147 @@ var gm = require('gm').subClass({
 var fs = require('fs');
 var util = require('util');
 
-// get reference to S3 client
-
 var s3 = new AWS.S3();
 
 exports.handler = function(event, context) {
   var srcBucket = event.bucket.name;
   var dstBucket = srcBucket;
   var rotationDegree = event.rotation
-  var deleteFile = event.destroy
+  var deleteLocation = event.delete_location
 
-  // Object key may have spaces or unicode non-ASCII characters.
-  var srcKey = decodeURIComponent(event.location.replace(/\+/g, " "));
+  if(event.location) {
+    var srcKey = decodeURIComponent(event.location.replace(/\+/g, " "));
+    var typeMatch = srcKey.match(/\.([^.]*)$/);
+    var fileName = path.basename(srcKey);
 
-  var _75px = { width: 75, dstnKey: srcKey, destinationPath: "thumb" };
-  var _200px = { width: 200, dstnKey: srcKey, destinationPath: "medium" };
-  var _600px = { width: 600, dstnKey: srcKey, destinationPath: "large" };
+    if (!typeMatch) {
+      console.error('unable to infer image type for key ' + srcKey);
+      return;
+    }
+
+    var imageType = typeMatch[1].toLowerCase();
+
+    if (imageType != "jpg" && imageType != "gif" && imageType != "png" && imageType != "eps") {
+      console.log('skipping non-image ' + srcKey);
+      return;
+    }
+  }
+
+  var _75px = { width: 75, destinationPath: "thumb" };
+  var _200px = { width: 200, destinationPath: "medium" };
+  var _600px = { width: 600, destinationPath: "large" };
 
   var _sizesArray = [_75px, _200px, _600px];
 
-  // Infer the image type.
-  var typeMatch = srcKey.match(/\.([^.]*)$/);
-  var fileName = path.basename(srcKey);
-
-  if (!typeMatch) {
-    console.error('unable to infer image type for key ' + srcKey);
-    return;
+  function download(srcBucket, srcKey) {
+    console.log("downloading")
+    return new Promise(function(resolve, reject) {
+      s3.getObject({
+        Bucket: srcBucket,
+        Key: srcKey
+      }, function(err, response) {
+        if(err) {
+          reject(err);
+        }
+        else {
+          resolve(response);
+        }
+      })
+    })
   }
 
-  var imageType = typeMatch[1].toLowerCase();
-
-  if (imageType != "jpg" && imageType != "gif" && imageType != "png" && imageType != "eps") {
-    console.log('skipping non-image ' + srcKey);
-    return;
+  function convert(response) {
+    console.log("converting")
+    return new Promise(function(resolve, reject) {
+      gm(response.Body).background('#FFFFFF').gravity('Center').strip().interlace('Plane').samplingFactor(2, 1).toBuffer('JPG', function(err, buffer) {
+        if(err) {
+          reject(err);
+        }
+        else {
+          resolve(buffer);
+        }
+      })
+    })
   }
 
-  if(deleteFile) {
-    _sizesArray.forEach(function(v) {
-      pathWithFolder = srcKey.split('/').slice(0, 5).join('/');
-
-      if(deleteFile == true) {
-        deletePath = pathWithFolder + "/" + v.destinationPath + "/" + fileName.slice(0, -4) + ".jpg";
-      } else {
-        deletePath = pathWithFolder + "/" + v.destinationPath + "/" + deleteFile;
-      }
-
-      console.log(deletePath);
-
-      s3.deleteObject({
-          Bucket: dstBucket,
-          Key: deletePath,
-      }, function(err, data) {
+  function process(response, width, style) {
+    console.log("processing")
+    return new Promise(function(resolve, reject) {
+      gm(response).size(function(err, size) {
         if(err) {
           console.log(err);
+          return reject(err);
         }
-      });
-    });
 
-    if(deleteFile == true) { return; }
+        var resized = this.resize(width, width, "!");
+
+        if(style == "large") { resized = resized.fill("#FFFFFF").fontSize(30).drawText(10, 10, "MeraYog.com", "SouthWest"); }
+        if(style == "medium") { resized = resized.extent([_200px['width'], _200px['width']]) }
+        if (rotationDegree) { resized = resized.rotate("#FFFFFF", rotationDegree) }
+
+        resized.toBuffer('JPG', function(err, buffer) {
+          if(err) { reject(err) }
+          else { resolve(buffer) }
+        })
+      })
+    })
   }
 
-  async.forEachOf(_sizesArray, function(value, key, callback) {
-    async.waterfall([
-      function download(next) {
-        s3.getObject({
-          Bucket: srcBucket,
-          Key: srcKey
-        }, next);
-      },
+  function upload(data, style) {
+    console.log("uploading")
+    pathWithFolder = srcKey.split('/').slice(0, 5).join('/')
 
-      function convert(response, next) {
-        gm(response.Body).antialias(true).density(300).toBuffer('JPG', function(err, buffer) {
-          if (err) {
-            next(err);
-          } else {
-            next(null, buffer);
-          }
-        });
-      },
-
-      function process(response, next) {
-        gm(response).size(function(err, size) {
-          if(err) { console.log(err); return; }
-
-          var width = _sizesArray[key].width;
-          var height = width;
-          var index = key;
-
-          if(key == 2) {
-            var resized = this.resize(width, height, "!").fill("#FFFFFF").fontSize(30).drawText(10, 10, "MeraYog.com", "SouthWest");
-            //var resized = this.resize(width, height).gravity("SouthWest").draw(['image over 0,0 0,0 /var/task/watermark.png'])
-          } else {
-            var resized = this.resize(width, height, "!");
-          }
-          console.log(rotationDegree);
-
-          if(rotationDegree) {
-            resized = resized.rotate("#FFFFFF", rotationDegree)
-          }
-
-          resized.background('#FFFFFF').gravity('Center').noProfile().interlace('Plane').samplingFactor(2, 1).toBuffer(
-            'JPG', function(err, buffer) {
-              console.log("Printing the buffer...");
-              console.log(buffer);
-
-              if (err) {
-                next(err);
-              } else {
-                next(null, buffer, key);
-              }
-            });
-        });
-      },
-
-      function upload(data, index, next) {
-        console.time("uploadImage");
-        pathWithFolder = srcKey.split('/').slice(0, 5).join('/')
-        console.log("upload to path : " + pathWithFolder + "/" + _sizesArray[index].destinationPath + "/" + fileName.slice(0, -4) + ".jpg");
-
-        s3.putObject({
-          Bucket: dstBucket,
-          Key: pathWithFolder + "/" + _sizesArray[index].destinationPath + "/" + fileName.slice(0, -4) + ".jpg",
-          Body: data,
-          ContentType: 'JPG',
-          ACL: 'public-read'
-        }, next);
-        console.timeEnd("uploadImage");
+    s3.putObject({
+      Bucket: dstBucket,
+      Key: pathWithFolder + "/" + style + "/" + fileName.slice(0, -4) + ".jpg",
+      Body: data,
+      ContentType: 'JPG',
+      ACL: 'public-read'
+    }, function(err, data) {
+      if(err) {
+        console.log(err);
       }
-    ], function(err, result) {
-      if (err) {
-        console.error(err);
-      }
-
-      console.log("End of step " + key);
-      callback();
     });
-  }, function(err) {
-    if (err) {
-      console.error('---->Unable to resize ' + srcBucket + '/' + srcKey + ' and upload to ' + dstBucket +
-                    '/images' + ' due to an error: ' + err);
-    } else {
-      console.log('---->Successfully resized ' + srcBucket + ' and uploaded to' + dstBucket + "/images");
-    }
-    context.done();
-  });
-};
+  }
+
+  function deleteFile(srcBucket, deletePath) {
+    console.log("deleting");
+
+    s3.deleteObject({
+      Bucket: srcBucket,
+      Key: deletePath,
+    }, function(err, data) {
+      if(err) {
+        console.log(err);
+      }
+    })
+  }
+
+  if(deleteLocation) {
+    _sizesArray.forEach(function(v) {
+      pathWithFolder = deleteLocation.split('/').slice(0, 5).join('/');
+      fileToDelete = path.basename(deleteLocation);
+      deletePath = pathWithFolder + "/" + v.destinationPath + "/" + fileToDelete;
+
+      deleteFile(srcBucket, deletePath);
+    });
+  }
+
+  if(srcKey) {
+    download(srcBucket, srcKey).then(function(response) {
+      return convert(response);
+    }).then(function(response) {
+      return Promise.all(_sizesArray.map(function(v) {
+        return process(response, v.width, v.destinationPath).then(function(data) {
+          upload(data, v.destinationPath);
+        })
+      }))
+    }).then(function(results) {
+      console.log(results);
+      if (results.length == 3) { context.done() }
+    }).catch(function(error) {
+      console.log("catching");
+      console.log(error);
+    });
+  }
+}
